2ff73ddccf9951e5b67c751358cb4c8467b28a5f
ThomasHarb <harbonnier.th@gmail.com>
2024-02-25 23:11:32 +0100

    Change Topbar display

--- src/layouts/Main/components/Topbar/Topbar.js
+++ src/layouts/Main/components/Topbar/Topbar.js
@@ -1,100 +1,192 @@
-import React from "react";
-import PropTypes from "prop-types";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Link from "@mui/material/Link";
+import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
 import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import InsightsIcon from "@mui/icons-material/Insights";
 import Photo from "../../../../svg/photoThomas.jpeg";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import { Typography } from "@mui/material";
-import { useMediaQuery } from "@mui/material";
-/* import { Typography } from "@mui/material"; */
+import Link from "@mui/material/Link";
 
-const Topbar = ({ onSidebarOpen }) => {
-  const isMobile = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+function ResponsiveAppBar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
-    <Box
-      display={"flex"}
-      justifyContent={"space-between"}
-      alignItems={"center"}
-      width={"100%"}
-    >
-      <Box display={"flex"} alignItems={"center"}>
-        <Box marginRight={{ xs: 1, sm: 2 }}>
-          {/*           <IconButton onClick={onSidebarOpen} aria-label="Menu">
-            <Typography variant="h6" component="div" color={'textSecondary'}>
-              Skills
-            </Typography> */}
-          {/*<MenuIcon />*/}
-          {/* </IconButton> */}
-        </Box>
-        {!isMobile && (
-          <Box
-            display={"flex"}
-            alignItems="baseline"
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <InsightsIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
             component="a"
-            underline="none"
-            href="https://thomasharb.github.io/home/"
-            title="THarbonnier"
-            height={{ xs: 28, md: 32 }}
-            width={45}
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
           >
-            <Avatar alt="Thomas Harbonnier" src={Photo} />
-          </Box>
-        )}
-      </Box>
-      <Box display="flex" flexGrow={1} marginLeft={2}>
-        {isMobile && (
-          <Typography>
-            <h5>
-              Thomas Harbonnier <br />
-              Python Developer
-              <br />| Data Analyst
-            </h5>
+            T.Harbonnier
           </Typography>
-        )}
-        {!isMobile && (
-          <Typography justifyContent={"center"}>
-            Thomas Harbonnier
-            <br />
+          <Typography
+            justifyContent={"center"}
+            sx={{ display: { xs: "none", md: "flex" } }}
+          >
             Python Developer | Data Analyst
           </Typography>
-        )}
-      </Box>
-      <Box display="flex" alignItems={"center"}>
-        <Box>
-          <IconButton
-            aria-label="Linkedin.com"
-            onClick={() => window.open("https://www.Linkedin.com")}
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              <MenuItem onClick={handleCloseUserMenu}>
+                <Link
+                  underline="none"
+                  component="a"
+                  href="https://thomasharb.github.io/home/"
+                  color="textSecondary"
+                >
+                  Home
+                </Link>
+              </MenuItem>
+              <MenuItem onClick={handleCloseUserMenu}>
+                <Link
+                  underline="none"
+                  component="a"
+                  href="https://thomasharb.github.io/netflix-project/"
+                  color="textSecondary"
+                >
+                  Movies Project
+                </Link>
+              </MenuItem>
+              <MenuItem onClick={handleCloseUserMenu}>
+                <Link
+                  underline="none"
+                  component="a"
+                  href="https://thomasharb.github.io/fastp-project/"
+                  color="textSecondary"
+                >
+                  Fastp Dashboard Project
+                </Link>
+              </MenuItem>
+              <MenuItem onClick={handleCloseUserMenu}>
+                <Link
+                  underline="none"
+                  component="a"
+                  href="https://thomasharb.github.io/contact/"
+                  color="textSecondary"
+                >
+                  Contact
+                </Link>
+              </MenuItem>
+            </Menu>
+          </Box>
+          <InsightsIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
           >
-            <LinkedInIcon fontSize="large" />
-          </IconButton>
-        </Box>
-        <Box marginX={2}>
-          <IconButton
-            aria-label="Github.com"
-            onClick={() => window.open("https://github.com")}
+            T.Harbonnier
+          </Typography>
+          <Box
+            sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}
+            marginLeft={8}
           >
-            <GitHubIcon fontSize="large" />
-          </IconButton>
-        </Box>
-        <Box
-          sx={{ display: { xs: "flex", md: "flex" }, flexWrap: "wrap" }}
-          alignItems={"center"}
-        >
-          <Box flexGrow={1} marginRight={2}>
             <Link
               underline="none"
               component="a"
               href="https://thomasharb.github.io/home/"
-              color="textPrimary"
+              color="textSecondary"
             >
               Home
             </Link>
+            <Link
+              underline="none"
+              component="a"
+              href="https://thomasharb.github.io/netflix-project/"
+              color="textSecondary"
+              marginLeft={4}
+            >
+              Movies Project
+            </Link>
+            <Link
+              underline="none"
+              component="a"
+              href="https://thomasharb.github.io/fastp-project/"
+              color="textSecondary"
+              marginLeft={4}
+            >
+              Fastp Dashboard Project
+            </Link>
           </Box>
-          <Box>
+          <Box
+            sx={{ flexGrow: 0, display: { xs: "none", md: "flex" } }}
+            marginRight={4}
+          >
             <Button
               variant="contained"
               color="primary"
@@ -106,18 +198,79 @@ const Topbar = ({ onSidebarOpen }) => {
               Contact me
             </Button>
           </Box>
-        </Box>
-      </Box>
-    </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Thomas Harbonnier" src={Photo} />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {/*               {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))} */}
+              <MenuItem onClick={handleCloseUserMenu}>
+                <Link
+                  underline="none"
+                  component="a"
+                  href="https://thomasharb.github.io/home/"
+                  color="textSecondary"
+                >
+                  Home
+                </Link>
+              </MenuItem>
+              <MenuItem onClick={handleCloseUserMenu}>
+                <Link
+                  underline="none"
+                  component="a"
+                  href="https://thomasharb.github.io/netflix-project/"
+                  color="textSecondary"
+                >
+                  Movies Project
+                </Link>
+              </MenuItem>
+              <MenuItem onClick={handleCloseUserMenu}>
+                <Link
+                  underline="none"
+                  component="a"
+                  href="https://thomasharb.github.io/fastp-project/"
+                  color="textSecondary"
+                >
+                  Fastp Dashboard Project
+                </Link>
+              </MenuItem>
+              <MenuItem onClick={handleCloseUserMenu}>
+                <Link
+                  underline="none"
+                  component="a"
+                  href="https://thomasharb.github.io/contact/"
+                  color="textSecondary"
+                >
+                  Contact
+                </Link>
+              </MenuItem>
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
-};
-
-Topbar.propTypes = {
-  onSidebarOpen: PropTypes.func,
-  themeToggler: PropTypes.func.isRequired,
-  themeMode: PropTypes.string.isRequired,
-  setThemePalette: PropTypes.func.isRequired,
-  paletteType: PropTypes.string.isRequired,
-};
-
-export default Topbar;
+}
+export default ResponsiveAppBar;
